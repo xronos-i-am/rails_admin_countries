89a26743f654f03a8e642482ce9a06a4b719aee8
Sergey Malykh <xronos.i.am@gmail.com>
2015-06-04 09:57:06 +0300

    integration fixes

--- lib/rails_admin_countries.rb
+++ lib/rails_admin_countries.rb
@@ -1,5 +1,8 @@
 require "rails_admin_countries/version"
 
+require "countries"
+
+require "rails_admin_countries/engine"
 require "rails_admin_countries/mongoid" if defined?(Mongoid)
 require "rails_admin_countries/country_field"
 
--- lib/rails_admin_countries/version.rb
+++ lib/rails_admin_countries/version.rb
@@ -1,3 +1,3 @@
 module RailsAdminCountries
-  VERSION = "0.1.0"
+  VERSION = "1.0.0"
 end
